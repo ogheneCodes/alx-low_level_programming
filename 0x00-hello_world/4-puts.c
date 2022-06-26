16aed228450ca66cfde6b56d8372bff77611b004
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-06-26 09:57:08 -0700

    fourth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*Entry to main entry poin main
+**
+**Return 0
+*/
+int main(void)
+{
+	puts("\programming is like building a multilingual puzzle");
+	puts("\n");
+	return (0);
+}
