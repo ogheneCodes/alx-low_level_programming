34de803e09ff4dd7d5a7a8103d91bd094b1d0a7c
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-06-26 10:00:42 -0700

    fourt commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 **
 **Return 0
 */
-int main(void)
-{
-	puts("\programming is like building a multilingual puzzle");
-	puts("\n");
-	return (0);
-}
+	int main(void)
+	{
+		puts("\n");
+		puts("\programming is like building a multilingual puzzle");
+		return (0);
+	}
