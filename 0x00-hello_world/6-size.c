6160886ddb67d6f0f82488f73f3f395e6796805c
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-06-26 15:24:19 -0700

    sixth commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-  
+{ 
   int a;
   long int b;
   long long int c;
@@ -17,6 +16,5 @@ int main(void)
   printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
   printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
   printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-  return (0);
-  
+  return (0); 
 }
