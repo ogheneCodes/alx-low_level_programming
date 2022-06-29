509694cef6f8962d44d6f88557bfe20ad4d3f294
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-06-29 14:54:54 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
-22:46
-#include <stdio.h>
+
+
 /**
  * main - Entry point
  *
