295562db2ed6a3fcfc2dccaf85326ad5ab3d7475
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-07-07 05:57:32 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_most_numbers -> this prints most numbers
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
